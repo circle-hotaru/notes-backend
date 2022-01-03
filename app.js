37e2f2a1b6ef788a4b3de34556eec2b8300d0a49
circlehotarux <mightyherox@gmail.com>
2022-01-03 21:50:07 +0800

    chore: add test route

--- app.js
+++ app.js
@@ -28,6 +28,11 @@ app.use('/api/login', loginRouter)
 app.use('/api/notes', notesRouter)
 app.use('/api/users', usersRouter)
 
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
+
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
